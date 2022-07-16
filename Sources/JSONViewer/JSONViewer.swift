03bd5673c2193e1a63585c00232cdf23fc87a5dc
Ben Gottlieb <ben@standalone.com>
2022-07-15 19:22:59 -0500

    initial code commit

--- Sources/JSONViewer/JSONViewer.swift
+++ Sources/JSONViewer/JSONViewer.swift
@@ -1,6 +1,16 @@
-public struct JSONViewer {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
-    }
+@available(macOS 10.15, iOS 13.0, *)
+public struct JSONView: View {
+	let json: Data
+	
+	public init(json: Data) {
+		self.json = json
+	}
+	
+	public var body: some View {
+		ScrollView() {
+			Text(String(data: json, encoding: .utf8) ?? "Unable to display")
+		}
+	}
 }
